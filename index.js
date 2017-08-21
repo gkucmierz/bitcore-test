dfb0e6211b52ac241a5590bba3babb4015a1134c
gkucmierz <gkucmierz@gmail.com>
2017-08-21 03:27:37 +0200

    bitcore message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const bitcore = require('bitcore-lib');
-const Mnemonic = require('bitcore-mnemonic');
+// const Mnemonic = require('bitcore-mnemonic');
+const Message = require('bitcore-message');
 
-console.log(Object.keys(bitcore));
-console.log(Object.keys(Mnemonic));
+// console.log(Object.keys(bitcore));
+// console.log(Object.keys(Mnemonic));
